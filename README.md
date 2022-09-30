c428796dee85bbeb84926a9da75ed95985405f64
Yuri Rossi Tonin <yurirt94@gmail.com>
2022-09-30 18:15:42 -0300

    Left only build content

--- README.md
+++
@@ -1,5 +0,0 @@
-# Website
-
-This website is built using [Docusaurus 2](https://v2.docusaurus.io/), a modern static website generator.
-
-Under construction!
--- src/hooks/useFullUrl.js
+++
@@ -1,9 +0,0 @@
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import useBaseUrl from '@docusaurus/useBaseUrl';
-
-export default function useFullUrl(uri) {
-  const { siteConfig } = useDocusaurusContext();
-  let { url } = siteConfig || {};
-  
-  return `${url}${useBaseUrl(uri)}`;
-}
