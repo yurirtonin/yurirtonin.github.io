9c49410f4d1225af12f90ff0bee5602c19059d61
Yuri Rossi Tonin <yurirt94@gmail.com>
2022-10-01 00:11:02 -0300

    Deploy website - based on b30033c5fa3075ec47c34e43f72b7ab590205375

--- pt-BR/assets/js/e38a50e9.0c30d637.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[29],{1182:function(e){e.exports=JSON.parse('{"permalink":"/pt-BR/blog","page":1,"postsPerPage":3,"totalPages":1,"totalCount":1,"previousPage":null,"nextPage":null,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
